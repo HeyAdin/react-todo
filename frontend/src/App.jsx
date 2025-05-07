db29758ab91b3a731015f79830585a4e24416970
heyAdin <adinahmad003@gmail.com>
2025-05-07 23:31:34 +0530

    addedn component to list todos

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,21 @@
+import { useEffect, useState } from "react"
 import { CreateTodo } from "./component/CreateTodo"
-
+import { Todos } from "./component/Todos"
 function App() {
-
+  const [todos, setTodos] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:3002/todo")
+      .then((response) => {
+        response.json()
+          .then((data) => {
+            setTodos(data);
+          })
+      });
+  }, [])
   return (
     <>
       <CreateTodo />
+      <Todos todos={todos} />
     </>
   )
 }
---
+++ frontend/src/component/Todos.jsx
@@ -0,0 +1,10 @@
+export  function Todos({todos}) {
+
+    return todos.map((todo) => {
+            return <div>
+                    <h1>{todo.title}</h1>
+                    <h2>{todo.description}</h2>
+                    <button>{todo.completed == true ? "Done" : "Mark as done"}</button>
+                </div>
+        })
+}
